244735761b8d67bc2ec07e5111b4314e64a63b1d
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-05 20:37:45 +0300

    implemented task and tests

--- readme.md
+++ readme.md
@@ -1,5 +1,21 @@
-# Boilerplate for JS tasks
+# The "Bulls & Cows" game
 
 **Read the guideline before start**
 
 [Guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md)
+
+## Rules
+
+- computed makes a number of 4 different digits
+- player enters a number of 4 different digits too
+- computer compares given numbers and shows a result of 'bulls' and 'cows'
+- `bull` - guessed digit is on its place (index)
+- `cow` - guessed digit presents in the number but place (index) is wrong
+
+## Example
+Computer makes `1234`, user prints `1345`. The result is one `bull` (guessed
+digit `1` is on it's place) and 2 `cows` (digits `3` and `4` are present but on
+wrong places). 
+
+## Additional scripts
+`npm run play` - run game
---
+++ src/startGame.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const readline = require('readline');
+const bullsAndCows = require('./bullsAndCows');
+
+const generateRandomDigit = () => `${Math.floor(Math.random() * 10)}`;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter 4 digits number: ', (enteredNumber) => {
+  let generatedNumber = '';
+  for (let i = 0; i < 4; i += 1) {
+    let digit = generateRandomDigit();
+    while (generatedNumber.includes(digit)) {
+      digit = generateRandomDigit();
+    }
+    generatedNumber += digit;
+  }
+
+  const result = bullsAndCows(generatedNumber, enteredNumber);
+
+  let answer = '';
+
+  if (typeof result === 'undefined') {
+    answer = 'Wrong input. Please enter 4 digits number.';
+  } else {
+    const [bulls, cows] = result;
+
+    answer = `The result is ${bulls} bull(s) and ${cows} cow(s)`;
+  }
+
+  console.log(
+    `Computer generated ${generatedNumber}, you entered ${enteredNumber}.
+    ----
+    ${answer}
+    `
+  );
+
+  rl.close();
+});

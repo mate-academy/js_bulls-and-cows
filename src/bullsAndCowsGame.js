9f02d8e34b8d0776489254df7b8df03c664277dd
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-23 13:06:35 +0200

    fix mistakes

--- src/bullsAndCowsGame.js
+++ src/bullsAndCowsGame.js
@@ -2,7 +2,7 @@
 
 function bullsAndCowsGame() {
   function enteredFourDigits() {
-    const entered = window.prompt('Enter 4-digit number with different digits',
+    const entered = window.prompt('Enter your number',
       1234);
 
     if (entered === null) {
@@ -18,7 +18,7 @@ function bullsAndCowsGame() {
     for (let k = 0; k < entered.length; k++) {
       for (let j = k + 1; j < entered.length; j++) {
         if (entered[k] === entered[j]) {
-          window.alert('Wrong number! Input number again.');
+          window.alert('Wrong number! Digits should not be repeated.');
 
           return undefined;
         }
@@ -40,37 +40,18 @@ function bullsAndCowsGame() {
     enteredNum = enteredFourDigits();
   }
 
-  function random(min, max) {
-    return Math.floor(
-      Math.random() * (max - min + 1) + min
-    );
-  }
-
   function generateNumberByComp() {
-    let result = '';
+    let numbers = '';
 
-    const firstNum = random(0, 9);
-    let secondNum;
-    let thirdNum;
-    let fourthNum;
+    while (numbers.length < 4) {
+      const number = Math.trunc(Math.random() * 10);
 
-    do {
-      secondNum = random(0, 9);
-    } while (firstNum === secondNum);
+      if (!numbers.includes(String(number))) {
+        numbers += number;
+      }
+    }
 
-    do {
-      thirdNum = random(0, 9);
-    } while (thirdNum === firstNum || thirdNum === secondNum);
-
-    do {
-      fourthNum = random(0, 9);
-    } while (fourthNum === firstNum
-      || fourthNum === secondNum
-      || fourthNum === thirdNum);
-
-    result = result + firstNum + secondNum + thirdNum + fourthNum;
-
-    return result;
+    return numbers;
   }
 
   const generatedNum = generateNumberByComp();
@@ -93,17 +74,11 @@ function bullsAndCowsGame() {
     };
 
     if (generatedNum === enteredNum) {
-      window.alert('You win!!!\nBulls: '
-      + resultObj.bulls
-      + ', Cows: '
-      + resultObj.cows);
+      window.alert(`You win!!!
+Bulls: ${resultObj.bulls}, Cows: ${resultObj.cows}`);
     } else {
-      window.alert('Your number: '
-      + enteredNum
-      + '\nBulls: '
-      + resultObj.bulls
-      + ', Cows: '
-      + resultObj.cows);
+      window.alert(`Your number: ${enteredNum}
+Bulls: ${resultObj.bulls}, Cows: ${resultObj.cows}`);
     }
   }
 

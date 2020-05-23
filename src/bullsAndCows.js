95e4ca10c643e53e6e79fe7a78ca7ad3f0644e8b
solaryasha <solaryasha@gmail.com>
2020-05-23 10:57:41 +0300

    My solution

--- src/bullsAndCows.js
+++ src/bullsAndCows.js
@@ -27,6 +27,42 @@
  */
 function bullsAndCows(generatedNumber, enteredNumber) {
   // write code here
+  const storage = {};
+  const regex = /^\d{4}$/;
+  let cows = 0;
+  let bulls = 0;
+
+  if (!(regex.test(enteredNumber))) {
+    return undefined;
+  }
+
+  for (let i = 0; i < generatedNumber.length; i++) {
+    storage[generatedNumber[i]] = i;
+  }
+
+  for (let i = 0; i < enteredNumber.length; i++) {
+    const currentValue = enteredNumber[i];
+    // check for repeating numbers
+
+    if (currentValue === enteredNumber[i + 1]) {
+      return undefined;
+    }
+
+    if (storage.hasOwnProperty(currentValue)) {
+      if (storage[currentValue] === i) {
+        bulls++;
+      } else {
+        cows++;
+      }
+    }
+  }
+
+  const yourAnswer = {
+    'bulls': bulls,
+    'cows': cows,
+  };
+
+  return yourAnswer;
 }
 
 module.exports = bullsAndCows;
